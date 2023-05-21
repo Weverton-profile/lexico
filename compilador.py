ac8a65ad4e8dea5169fb910498328c9d1a4757e2
Weverton-profile <wevertonalves.profile@gmail.com>
2023-05-21 03:08:37 -0300

    mudou muita coisa

--- compilador.py
+++ compilador.py
@@ -1,243 +1,237 @@
-import re
 import string
 
-def identificadores(entrada, estado):
-    for i in range(len(entrada)):
-        caractere = entrada[i]
-        if estado != -1:
-            if estado == 0:
-                if (caractere in string.ascii_lowercase or 
-                    caractere in string.ascii_uppercase):
-                    estado = 1
-                else:
-                    estado = -1
-            elif estado == 1:
-                if (caractere in [str(x) for x in range(0, 9)] or 
-                    caractere in string.ascii_lowercase):
-                    estado = 2
-                elif caractere == "_":
-                    estado = 3
-                elif caractere == "$":
-                    estado = 4
-                else:
-                    estado = -1
-            elif estado == 3:
-                if caractere in string.ascii_lowercase or caractere in [str(x) for x in range(0, 9)]:
-                    estado = 2
-            elif estado == 4:
-                if caractere in string.ascii_lowercase:
-                    estado = 2
-                else:
-                    estado = -1
-            elif estado == 2:
-                if (caractere in [str(x) for x in range(0, 9)] or 
-                    caractere in string.ascii_lowercase):
-                    estado = 2
-                elif caractere == "$":
-                    estado = 4
-                else:
-                    estado = -1
-        else:
+palavras_reservadas = ['program', 'if', 'then', 'else', 'while', 'do', 'until', 'repeat', 'int', 'double', 'char', 'case', 'switch', 'end', 'procedure', 'function', 'for', 'begin']
+
+def geral():
+  tokens = []
+  buffer = ''
+  comentario = False
+  estado = 0
+  with open('teste.txt', 'r') as arquivo:
+    linhas = arquivo.readlines()
+  for linha in linhas:
+    for i in range(len(linha)):
+      token = linha[i]
+      if comentario == False:
+        if estado == 0:
+          if (token in string.ascii_lowercase or 
+              token in string.ascii_uppercase):
+            buffer += token
+            estado = 1
+          elif token == "-":
+            buffer += token
+            estado = 11
+          elif token in [str(x) for x in range(0, 9)]:
+            buffer += token
+            estado = 12
+          elif token in [';', ',', '.', '(', ')', '{', '}', '=']:
+            print(f'<SIMBOLO_ESPECIAL, {token}>')
+            estado = 0
+          elif token == "<":
+            buffer += token
+            estado = 16
+          elif token in ['>', ':', '*', '+']:
+            buffer += token
+            estado = 17
+          elif token == "@":
+            buffer += token
+            estado = 5
+          elif token == "/":
+            buffer += token
+            estado = 7
+          else:
+            if token == ' ' or token == '\n':
+              continue
+            else:
+              print(f'Erro, caractere não reconhecido: "{token}".')
+              break
+        elif estado == 1:
+          if (token in [str(x) for x in range(0, 9)] or 
+              token in string.ascii_lowercase):
+              buffer += token
+              estado = 2
+          elif token == "_":
+            buffer += token
+            estado = 3
+          elif token == "$":
+            buffer += token
+            estado = 4
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<IDENTIFICADOR, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 3:
+          if token in string.ascii_lowercase or token in [str(x) for x in range(0, 9)]:
+            buffer += token
+            estado = 2
+          elif token == ' ' or token == '\n':
+            print(f'Erro, você tem "{buffer}", "{buffer[-1]}" não pode finalizar um identificador, complete com o que é esperado.')
             break
-    if estado == 2 or estado == 1:
-        return True
-    else:
-        return False
-
-def comentarios(entrada, estado):
-    for i in range(len(entrada)):
-        caractere = entrada[i]
-        if estado != -1:
-            if estado == 0:
-                if caractere == "@":
-                    estado = 5
-                elif caractere == "/":
-                    estado = 7
-                else:
-                    estado = -1
-            elif estado == 5:
-                if caractere == "@":
-                    estado = 6
-                else:
-                    estado = -1
-            elif estado == 6:
-                if i + 1 == len(entrada):
-                    estado = 25
-            elif estado == 7:
-                if caractere == "/":
-                    estado = 8
-                elif caractere == "*":
-                    estado = 22
-                else:
-                    estado = -1
-            elif estado == 8:
-                estado = 9
-            elif estado == 9:
-                if caractere == "/":
-                    estado = 10
-            elif estado == 10:
-                if caractere == "/":
-                    estado = 25
-                else:
-                    estado = 9
-            elif estado == 22:
-                estado = 23
-            elif estado == 23:
-                if caractere == "*":
-                    estado = 24
-                else:
-                    estado = 23
-            elif estado == 24:
-                if caractere == "/":
-                    estado = 25
-                else:
-                    estado = 23
-        else:
+          else:
+            print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
             break
-    if estado == 5 or estado == 25 or estado == 7:
-        return True
-    else:
-        return False
-
-def digitos(entrada, estado):
-    for i in range(len(entrada)):
-        caractere = entrada[i]
-        if estado != -1:
-            if estado == 0:
-                if caractere == "-":
-                    estado = 11
-                elif caractere in [str(x) for x in range(0, 9)]:
-                    estado = 12
-                else:
-                    estado = -1
-            elif estado == 11:
-                if caractere in [str(x) for x in range(0, 9)]:
-                    estado = 12
-                else:
-                    estado = -1
-            elif estado == 12:
-                if caractere in [str(x) for x in range(0, 9)]:
-                    estado = 12
-                elif caractere == ",":
-                    estado = 13
-                else:
-                    estado = -1
-            elif estado == 13:
-                if caractere in [str(x) for x in range(0, 9)]:
-                    estado = 14
-                else:
-                    estado = -1 
-            elif estado == 14:
-                if caractere in [str(x) for x in range(0, 9)]:
-                    estado = 14
-                else:
-                    estado = -1
-        else:
-            break     
-    if estado == 11 or estado == 12 or estado == 14:
-        return True
-    else:
-        return False
-
-def simbolo_especial(entrada, estado, simbolos_especiais):
-    for i in range(len(entrada)):
-        caractere = entrada[i]
-        if estado != -1:
-            if estado == 0:
-                if caractere in simbolos_especiais:
-                    estado = 15
-                elif caractere == "<":
-                    estado = 16
-                elif caractere == ">":
-                    estado = 17
-                elif caractere == "=":
-                    estado = 21
-                elif caractere == ":":
-                    estado = 18
-                elif caractere == "*":
-                    estado = 19
-                elif caractere == "+":
-                    estado = 20
-                else:
-                    estado = -1
-            elif estado == 16:
-                if caractere == "=":
-                    estado = 21
-                elif caractere == ">":
-                    estado = 21
-                else:
-                    estado = -1
-            elif estado == 17:
-                if caractere == "=":
-                    estado = 21
-                else:
-                    estado = -1
-            elif estado == 18:
-                if caractere == "=":
-                    estado = 21
-                else:
-                    estado = -1
-            elif estado == 19:
-                if caractere == "=":
-                    estado = 21
-                else:
-                    estado = -1
-            elif estado == 20:
-                if caractere == "=":
-                    estado = 21
-                else:
-                    estado = -1
-        else:
+        elif estado == 4:
+          if  token in string.ascii_lowercase:
+            buffer += token
+            estado = 2
+          elif token == ' ' or token == '\n':
+            print(f'Erro, você tem "{buffer}", "{buffer[-1]}" não pode finalizar um identificador, complete com o que é esperado.')
             break
-    if (estado == 15 or estado == 16 or estado == 17 or 
-        estado == 18 or estado == 19 or estado == 20 or estado == 21):
-        return True
-    else:
-        return False
-
-def analisador_lexico(linhas):
-    palavras_reservadas = ['program', 'if', 'then', 'else', 'while', 'do', 'until', 'repeat', 'int', 'double', 'char', 'case', 'switch', 'end', 'procedure', 'function', 'for', 'begin']
-    tabela_de_simbolos = [';', ',', '.', '(', ')', '{', '}', '+', '-', '*', '<', '>', '=', ':', '/', '@', ':=', '>=', '<=', '*=', '+=', '/*', '*/', '//', '@@']
-
-    padrao = r'//.*?\n|/\*.*?\*/|@@.*?\n|\w+|[^\w\s]+'
-    tokens = re.findall(padrao, linhas, re.DOTALL)
-
-    lexemes = []
-    comentario = False
-    for token in tokens:
-        if comentario:
-            if token.endswith('\n'):
-                comentario = False
-            continue
-        elif token.startswith('//') or token.startswith('@@'):
-            continue
-        elif token.startswith('/*'):
-            if '*/' not in token:
-                comentario = True
-            continue
-        elif token.endswith('*/'):
-            continue
-        elif token in palavras_reservadas:
-            lexemes.append(('PALAVRA_RESERVADA', token))
-        elif token in tabela_de_simbolos:
-            lexemes.append(('SIMBOLO', token))
-        elif identificadores(token, 0):
-            lexemes.append(('IDENTIFICADOR', token))
-        elif comentarios(token, 0):
-            lexemes.append(('COMENTARIO', token))
-        elif digitos(token, 0):
-            lexemes.append(('DIGITO', token))
-        elif simbolo_especial(token, 0, tabela_de_simbolos):
-            lexemes.append(('SIMBOLO_ESPECIAL', token))
-
-    return lexemes
-
-arquivo = open("padrao.txt", "r")
-
-linhas = arquivo.read()
-
-arquivo.close()
-
-result = analisador_lexico(linhas)
-for token, lexeme in result:
-    print(f'<{token}, {lexeme}>')
+          else:
+            print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+            break
+        elif estado == 2:
+          if (token in [str(x) for x in range(0, 9)] or 
+              token in string.ascii_lowercase):
+            buffer += token
+            estado = 2
+          elif token == "$":
+            buffer += token
+            estado = 4
+          else:
+            if token == ' ' or token == '\n':
+              if buffer in palavras_reservadas:
+                print(f'<PALAVRA_RESERVADA, {buffer}>')
+              else:
+                print(f'<IDENTIFICADOR, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 5:
+          if token == "@":
+            estado = 6
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 6:
+          buffer = ''
+          if token == '\n':
+            estado = 0
+        elif estado == 7:
+          if token == "/":
+            buffer += token
+            comentario = True
+            buffer = ''
+            estado = 8
+          elif token == "*":
+            buffer += token
+            comentario = True
+            buffer = ''
+            estado = 22
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 11:
+          if token in [str(x) for x in range(0, 9)]:
+            buffer += token
+            estado = 12
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 12:
+          if token in [str(x) for x in range(0, 9)]:
+            buffer += token
+            estado = 12
+          elif token == ",":
+            buffer += token
+            estado = 13
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<DIGITO, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 13:
+          if token in [str(x) for x in range(0, 9)]:
+            buffer += token
+            estado = 14
+          else:
+            if token == ' ' or token == '\n':
+              print(f'Erro, você tem "{buffer}", "{buffer[-1]}" não pode finalizar um digito, complete com o que é esperado.')
+              break
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 14:
+          if token in [str(x) for x in range(0, 9)]:
+            buffer += token
+            estado = 14
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<DIGITO, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 16:
+          if token == "=":
+            buffer += token
+            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+            buffer = ''
+            estado = 0
+          elif token == ">":
+            buffer += token
+            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+            buffer = ''
+            estado = 0
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+        elif estado == 17:
+          if token == "=":
+            buffer += token
+            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+            buffer = ''
+            estado = 0
+          else:
+            if token == ' ' or token == '\n':
+              print(f'<SIMBOLO_ESPECIAL, {buffer}>')
+              buffer = ''
+              estado = 0
+            else:
+              print(f'Erro, ocorrencia de um caractere inesperado: "{token}".')
+              break
+      else:
+        if estado == 8:
+          if token == "/":
+            estado = 9
+        elif estado == 9:
+          if token == "/":
+            comentario = False
+            estado = 0
+        elif estado == 22:
+          if token == "*":
+            estado = 23
+        elif estado == 23:
+          if token == "/":
+            comentario = False
+            estado = 0
