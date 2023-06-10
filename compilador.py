865bb7de00d111bd9685586dc0fa1acbb3e3ea7b
Weverton-profile <wevertonalves.profile@gmail.com>
2023-06-10 00:04:18 -0300

    updates

--- compilador.py
+++ compilador.py
@@ -2,216 +2,220 @@ import string
 
 palavras_reservadas = ['program', 'if', 'then', 'else', 'while', 'do', 'until', 'repeat', 'int', 'double', 'char', 'case', 'switch', 'end', 'procedure', 'function', 'for', 'begin']
 
-def geral():
-    buffer = ''
+def geral(arquivo):
+    token = ''
     comentario = False
     estado = 0
     mensagem = ''
     linha_erro = 0
-    with open('teste.txt', 'r') as arquivo:
+    with open(arquivo, 'r') as arquivo:
         linhas = arquivo.readlines()
     
     for linha in linhas:
         if estado != -1:
             i = 0
             linha_erro += 1
+            if '\n' not in linha:
+              linha += '\n'
             while i < len(linha):
-                token = linha[i]
+                caractere = linha[i]
                 if comentario == False:
                     if estado == 0:
-                        if (token in string.ascii_lowercase or 
-                            token in string.ascii_uppercase):
-                            buffer += token
+                        if (caractere in string.ascii_lowercase or 
+                            caractere in string.ascii_uppercase):
+                            token += caractere
                             estado = 1
-                        elif token == "-":
-                            buffer += token
+                        elif caractere == "-":
+                            token += caractere
                             estado = 11
-                        elif token in [str(x) for x in range(0, 9)]:
-                            buffer += token
+                        elif caractere in [str(x) for x in range(0, 9)]:
+                            token += caractere
                             estado = 12
-                        elif token in [';', ',', '.', '(', ')', '{', '}', '=']:
-                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                        elif caractere in [';', ',', '.', '(', ')', '{', '}', '=']:
+                            print(f'<SIMBOLO_ESPECIAL, {caractere}>')
                             estado = 0
-                        elif token == "<":
-                            buffer += token
+                        elif caractere == "<":
+                            token += caractere
                             estado = 16
-                        elif token in ['>', ':', '*', '+']:
-                            buffer += token
+                        elif caractere in ['>', ':', '*', '+']:
+                            token += caractere
                             estado = 17
-                        elif token == "@":
-                            buffer += token
+                        elif caractere == "@":
+                            token += caractere
                             estado = 5
-                        elif token == "/":
-                            buffer += token
+                        elif caractere == "/":
+                            token += caractere
                             estado = 7
-                        elif token == ' ' or token == '\n':
+                        elif caractere == '\t' or caractere == ' ' or caractere == '\n':
                             pass
                         else:
-                            mensagem = f'Erro linha {linha_erro}, caractere não reconhecido "{token}".'
+                            mensagem = f'Erro linha {linha_erro}, caractere não reconhecido "{caractere}".'
                             estado = -1
                     elif estado == 1:
-                        if (token in [str(x) for x in range(0, 9)] or 
-                            token in string.ascii_lowercase):
-                            buffer += token
+                        if (caractere in [str(x) for x in range(0, 9)] or 
+                            caractere in string.ascii_lowercase):
+                            token += caractere
                             estado = 2
-                        elif token == "_":
-                            buffer += token
+                        elif caractere == "_":
+                            token += caractere
                             estado = 3
-                        elif token == "$":
-                            buffer += token
+                        elif caractere == "$":
+                            token += caractere
                             estado = 4
                         else:
-                            print(f'<IDENTIFICADOR, {buffer}>')
-                            buffer = ''
+                            print(f'<IDENTIFICADOR, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 3:
-                        if token in string.ascii_lowercase or token in [str(x) for x in range(0, 9)]:
-                            buffer += token
+                        if caractere in string.ascii_lowercase or caractere in [str(x) for x in range(0, 9)]:
+                            token += caractere
                             estado = 2
                         else:
-                            mensagem = f'Erro linha {linha_erro}, você tem "{buffer}". "{buffer[-1]}" não pode finalizar um identificador.'
+                            mensagem = f'Erro linha {linha_erro}, você tem "{token}". "{token[-1]}" não pode finalizar um identificador.'
                             estado = -1
                     elif estado == 4:
-                        if token in string.ascii_lowercase:
-                            buffer += token
+                        if caractere in string.ascii_lowercase:
+                            token += caractere
                             estado = 2
                         else:
-                            mensagem = f'Erro linha {linha_erro}, você tem "{buffer}". "{buffer[-1]}" não pode finalizar um identificador.'
+                            mensagem = f'Erro linha {linha_erro}, você tem "{token}". "{token[-1]}" não pode finalizar um identificador.'
                             estado = -1
                     elif estado == 2:
-                        if (token in [str(x) for x in range(0, 9)] or 
-                            token in string.ascii_lowercase):
-                            buffer += token
+                        if (caractere in [str(x) for x in range(0, 9)] or 
+                            caractere in string.ascii_lowercase):
+                            token += caractere
                             estado = 2
                             if i + 1 == len(linha):
-                                if buffer in palavras_reservadas:
-                                    print(f'<PALAVRA_RESERVADA, {buffer}>')
+                                if token in palavras_reservadas:
+                                    print(f'<PALAVRA_RESERVADA, {token}>')
                                 else:
-                                    print(f'<IDENTIFICADOR, {buffer}>')
-                                buffer = ''
-                        elif token == "$":
-                            buffer += token
+                                    print(f'<IDENTIFICADOR, {token}>')
+                                token = ''
+                        elif caractere == "$":
+                            token += caractere
                             estado = 4
                         else:
-                            if buffer in palavras_reservadas:
-                                print(f'<PALAVRA_RESERVADA, {buffer}>')
+                            if token in palavras_reservadas:
+                                print(f'<PALAVRA_RESERVADA, {token}>')
                             else:
-                                print(f'<IDENTIFICADOR, {buffer}>')
-                            buffer = ''
+                                print(f'<IDENTIFICADOR, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 5:
-                        if token == "@":
+                        if caractere == "@":
                             estado = 6
                         else:
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 6:
-                        buffer = ''
-                        if token == '\n':
+                        token = ''
+                        if caractere == '\n':
                             estado = 0
                     elif estado == 7:
-                        if token == "/":
-                            buffer += token
+                        if caractere == "/":
+                            token += caractere
                             comentario = True
-                            buffer = ''
+                            token = ''
                             estado = 8
-                        elif token == "*":
-                            buffer += token
+                        elif caractere == "*":
+                            token += caractere
                             comentario = True
-                            buffer = ''
+                            token = ''
                             estado = 22
                         else:
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 11:
-                        if token in [str(x) for x in range(0, 9)]:
-                            buffer += token
+                        if caractere in [str(x) for x in range(0, 9)]:
+                            token += caractere
                             estado = 12
                         else:
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 12:
-                        if token in [str(x) for x in range(0, 9)]:
-                            buffer += token
+                        if caractere in [str(x) for x in range(0, 9)]:
+                            token += caractere
                             estado = 12
-                        elif token == ",":
-                            buffer += token
+                        elif caractere == ",":
+                            token += caractere
                             estado = 13
                         else:
-                            print(f'<DIGITO, {buffer}>')
-                            buffer = ''
+                            print(f'<DIGITO, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 13:
-                        if token in [str(x) for x in range(0, 9)]:
-                            buffer += token
+                        if caractere in [str(x) for x in range(0, 9)]:
+                            token += caractere
                             estado = 14
                         else:
-                            mensagem = f'Erro linha {linha_erro}, você tem "{buffer}". "{buffer[-1]}" não pode finalizar um dígito.'
+                            mensagem = f'Erro linha {linha_erro}, você tem "{token}". "{token[-1]}" não pode finalizar um dígito.'
                             estado = -1
                     elif estado == 14:
-                        if token in [str(x) for x in range(0, 9)]:
-                            buffer += token
+                        if caractere in [str(x) for x in range(0, 9)]:
+                            token += caractere
                             estado = 14
                             if i + 1 == len(linha):
-                                print(f'<DIGITO, {buffer}>')
+                                print(f'<DIGITO, {token}>')
                         else:
-                            print(f'<DIGITO, {buffer}>')
-                            buffer = ''
+                            print(f'<DIGITO, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 16:
-                        if token == "=":
-                            buffer += token
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                        if caractere == "=":
+                            token += caractere
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
-                        elif token == ">":
-                            buffer += token
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                        elif caractere == ">":
+                            token += caractere
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
                         else:
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                     elif estado == 17:
-                        if token == "=":
-                            buffer += token
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                        if caractere == "=":
+                            token += caractere
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
                         else:
-                            print(f'<SIMBOLO_ESPECIAL, {buffer}>')
-                            buffer = ''
+                            print(f'<SIMBOLO_ESPECIAL, {token}>')
+                            token = ''
                             estado = 0
                             i -= 1
                 else:
                     if estado == 8:
-                        if token == "/":
+                        if caractere == "/":
                             estado = 9
                     elif estado == 9:
-                        if token == "/":
+                        if caractere == "/":
                             comentario = False
                             estado = 0
                     elif estado == 22:
-                        if token == "*":
+                        if caractere == "*":
                             estado = 23
                     elif estado == 23:
-                        if token == "/":
+                        if caractere == "/":
                             comentario = False
                             estado = 0
-                
                 i += 1
         else:
-            print(mensagem)
             break
+    if mensagem != '':
+      print(mensagem)
+
+geral("teste.txt")
